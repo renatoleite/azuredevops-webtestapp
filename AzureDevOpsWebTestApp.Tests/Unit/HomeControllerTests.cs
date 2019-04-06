dae4b9bbacb651e4d8a4ea0f0f8fa389149d0998
Renato Leite <hr2.renato@gmail.com>
2019-04-06 14:10:26 -0300

    Commiting unit test and integration tests

---
+++ AzureDevOpsWebTestApp.Tests/Unit/HomeControllerTests.cs
@@ -0,0 +1,22 @@
+﻿using AzureDevOpsWebTestApp.Controllers;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Xunit;
+
+namespace AzureDevOpsWebTestApp.Tests.Unit
+{
+    public class HomeControllerTests
+    {
+        [Fact]
+        public void home_controller_get_should_return_string_content()
+        {
+            var controller = new HomeController();
+
+            var result = controller.Responding();
+
+            var contentResult = result as ContentResult;
+            contentResult.Should().NotBeNull();
+            contentResult.Content.Should().BeEquivalentTo("Responding");
+        }
+    }
+}
--- AzureDevOpsWebTestApp/Controllers/HomeController.cs
+++ AzureDevOpsWebTestApp/Controllers/HomeController.cs
@@ -19,5 +19,8 @@ namespace AzureDevOpsWebTestApp.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        [HttpGet("")]
+        public IActionResult Responding() => Content("Responding");
     }
 }
